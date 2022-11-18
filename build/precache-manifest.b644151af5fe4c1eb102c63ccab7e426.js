5eb770b5f89e8c27c404b8966cdb102720824de9
wu <wudi>
2022-11-18 14:37:24 +0800

    2022-11-18:14

---
+++ build/precache-manifest.b644151af5fe4c1eb102c63ccab7e426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956a8b8aef31aadf04537091175353a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a02465802a5ba85ec3d2",
+    "url": "./static/css/main.96e820db.chunk.css"
+  },
+  {
+    "revision": "61695347e6d0e3395b0b",
+    "url": "./static/js/2.9a41ac44.chunk.js"
+  },
+  {
+    "revision": "b579a4c7fb916e455762feb73d706be6",
+    "url": "./static/js/2.9a41ac44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02465802a5ba85ec3d2",
+    "url": "./static/js/main.3f9ace9e.chunk.js"
+  },
+  {
+    "revision": "7d9bf6668e7d41fffcff",
+    "url": "./static/js/runtime-main.aa675d43.js"
+  }
+]);
