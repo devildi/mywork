3c8366d383648ead114b8b4abd38e8ace7751128
wu <wudi>
2025-08-14 15:06:20 +0800

    25-8-14:1

---
+++ build/precache-manifest.bf67d50d1eb30b185cd7332e21d5f00d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14e90129611d1ac49717d432ec93a37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3dd7380d94feab19f4e",
+    "url": "./static/css/main.250c8eaa.chunk.css"
+  },
+  {
+    "revision": "b13799861bb7c27c2c95",
+    "url": "./static/js/2.aada2732.chunk.js"
+  },
+  {
+    "revision": "c51496dc0393831820e0becf68dde29c",
+    "url": "./static/js/2.aada2732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3dd7380d94feab19f4e",
+    "url": "./static/js/main.7040c574.chunk.js"
+  },
+  {
+    "revision": "7d9bf6668e7d41fffcff",
+    "url": "./static/js/runtime-main.aa675d43.js"
+  }
+]);
